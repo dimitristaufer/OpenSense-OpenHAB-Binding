d023dd989d9104993d0a686e9ea6b749123d3418
dimitristaufer <staufer@campus.tu-berlin.de>
2018-11-24 17:04:33 +0100

    Removed some jars, which prevented build

--- org.eclipse.smarthome.binding.opensensenetwork/src/main/java/org/eclipse/smarthome/binding/opensensenetwork/internal/OpenSenseNetworkHandler.java
+++ org.eclipse.smarthome.binding.opensensenetwork/src/main/java/org/eclipse/smarthome/binding/opensensenetwork/internal/OpenSenseNetworkHandler.java
@@ -235,23 +235,7 @@ public class OpenSenseNetworkHandler extends BaseThingHandler {
         lg = (BigDecimal) config.get("longitude");
         sensorID = (BigDecimal) config.get("sensorID");
 
-        // TODO: Initialize the handler.
-        // The framework requires you to return from this method quickly. Also, before leaving this method a thing
-        // status from one of ONLINE, OFFLINE or UNKNOWN must be set. This might already be the real thing status in
-        // case you can decide it directly.
-        // In case you can not decide the thing status directly (e.g. for long running connection handshake using WAN
-        // access or similar) you should set status UNKNOWN here and then decide the real status asynchronously in the
-        // background.
-
-        // set the thing status to UNKNOWN temporarily and let the background task decide for the real status.
-        // the framework is then able to reuse the resources from the thing handler initialization.
-        // we set this upfront to reliably check status updates in unit tests.
         updateStatus(ThingStatus.ONLINE);
 
-        // Note: When initialization can NOT be done set the status with more details for further
-        // analysis. See also class ThingStatusDetail for all available status details.
-        // Add a description to give user information to understand why thing does not work as expected. E.g.
-        // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
-        // "Can not access device as username and/or password are invalid");
     }
 }
